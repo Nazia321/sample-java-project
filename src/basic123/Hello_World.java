de4b36df466f478c39c0e6882980c7a9ecd06968
Nazia321 <naziaafreen18@gmail.com>
2022-03-11 18:55:33 +0530

    hello there

---
+++ src/basic123/Hello_World.java
@@ -0,0 +1,32 @@
+package basic123;
+
+public class Hello_World 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=20,c=30,d=40;
+		System.out.println(a+","+b+","+c+","+d);//10,20,30,40
+		//if we use (+) between two integer variables then it is an addition operator
+		//if we use(+) between one operand which is a string and other is integer then it is 
+		//a concatenation operator
+		
+	         
+
+	}
+
+}
+/*variable: 1)it is an alternative name for memory location
+            2)it is used to store the value
+            
+            Variable declaration
+            syntax:
+            datatype variablename;
+               int     a;
+               
+             Variable Initialization
+             
+             syntax:
+             datatype variablename=value;
+                 int     a       =  10;*/
+
